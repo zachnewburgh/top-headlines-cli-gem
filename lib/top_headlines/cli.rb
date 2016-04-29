72dcf5cb2e3508cd7a1426dd6bd1ebffd16147a8
zachnewburgh <zach.newburgh@gmail.com>
2016-04-29 16:04:35 -0400

    added while loop for exit, elsif to prevent exit input from generating 'error'

--- lib/top_headlines/cli.rb
+++ lib/top_headlines/cli.rb
@@ -10,30 +10,41 @@ class TopHeadlines::CLI
     end
     puts "\n"
     menu
+    goodbye
   end
 
   def menu
     puts "Which source do you want to view?"
-    input = gets.strip.upcase
-    if TopHeadlines::Source.all.keys.include?(input)
-      puts "\n*** #{input} ***"
-      TopHeadlines::Source.scrape_headlines(input).each_with_index do |headline, index|
-        puts "#{index + 1}. #{headline}"
-      end
-      puts "\n"
+    input = nil
+    while input != "EXIT"
+      input = gets.strip.upcase
+      if TopHeadlines::Source.all.keys.include?(input)
+        puts "\n*** #{input} ***"
+        TopHeadlines::Source.scrape_headlines(input).each_with_index do |headline, index|
+          puts "#{index + 1}. #{headline}"
+        end
+        puts "\n"
 
-      puts "Select article number to open."
-      num = gets.strip.to_i
-
-      while num > 0
-        url = TopHeadlines::Source.scrape_urls(input)[num-1]
-        system("open", url)
-        puts "Select another article number to open."
+        puts "Select article number to open."
         num = gets.strip.to_i
-      end
 
-    else
-      puts "error"
+        while num > 0
+          url = TopHeadlines::Source.scrape_urls(input)[num-1]
+          system("open", url)
+          puts "Select another article number to open."
+          num = gets.strip.to_i
+        end
+        puts "Invalid entry: select a source or type 'exit'."
+      elsif input != "EXIT"
+        puts "error"
+      else
+        nil
+      end
     end
+  end
+
+  def goodbye
+    puts "\nThanks for visiting – see you next time! :)"
+    puts "Copyright © 2016 Zach Newburgh"
   end
 end
