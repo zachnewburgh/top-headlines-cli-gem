88f1ab5294018da4dbe91f82d94fc89c777e3d54
zachnewburgh <zach.newburgh@gmail.com>
2016-04-29 21:12:21 -0400

    created #time and placed this above all scraping requests; editing return formatting of all_headlines; added sources option in menu, adding ascii graphic to #goodbye

--- lib/top_headlines/cli.rb
+++ lib/top_headlines/cli.rb
@@ -4,7 +4,7 @@ class TopHeadlines::CLI
     puts " ---------------------------"
     puts "| WELCOME TO TOP HEADLINES! |"
     puts " ---------------------------"
-    puts "Reporting live as of #{Time.now.strftime("%l:%M %p %Z on %a, %b #{Time.now.strftime("%e").to_i.ordinalize}, %Y")}"
+    time
     puts "\n"
     TopHeadlines::Source.all.keys.each do |source|
       puts "*** #{source} ***"
@@ -18,14 +18,29 @@ class TopHeadlines::CLI
     puts "Which source do you want to view? Alternatively, type 'all' to view all headlines or 'exit' to exit!"
     input = nil
     while input != "EXIT"
-      input = gets.strip.upcase
+      input = gets.strip.upcase 
       if input == "ALL"
+        puts " -------------------------------"
+        puts "| TOP HEADLINES & BREAKING NEWS |"
+        puts " -------------------------------"
+        time
         puts "\n"
         TopHeadlines::Source.all_headlines
         puts "\nSelect a source or type 'exit' to exit."
+      elsif
+        input == "SOURCES"
+        puts "\n"
+        puts "NEWS SOURCES"
+        puts "------------"
+        TopHeadlines::Source.all.keys.each do |source|
+          puts "*** #{source} ***"
+        end
+        puts "\nSelect a source or type 'exit' to exit."
       elsif TopHeadlines::Source.all.keys.include?(input)
         puts "\n*** #{input} ***"
-        TopHeadlines::Source.scrape_headlines(input).each_with_index do |headline, index|
+        time
+        puts "\n"
+        TopHeadlines::Source.scrape_headlines(input)[0,5].each_with_index do |headline, index|
           puts "#{index + 1}. #{headline}"
         end
         puts "\n"
@@ -48,8 +63,51 @@ class TopHeadlines::CLI
     end
   end
 
+  def time
+    puts "Reporting live as of #{Time.now.strftime("%l:%M %p %Z on %a, %b #{Time.now.strftime("%e").to_i.ordinalize}, %Y")}"
+  end
+
   def goodbye
     puts "\nThanks for visiting – see you next time! :)"
+
+    puts <<-DOC
+                                                                                                    
+                          `.`                                                                       
+                     ..` .-:-.                                                                      
+                    `:::--:::.                                                                      
+                 .---::::::::.                       ``````..``````                                 
+                `-:::::::::::``````             ``...---------------..```                           
+             .-..::::::::::::--:::-.        ``..-------------------------.``                        
+             .:::::::::::::::::::-.`      `..-------------------------------.`                      
+              -::::::::::::::::-`       `.------------------------------------.`                    
+              `-::::::::::::::-`      `.----------------------------------------.`                  
+               `-:::::::::::::.      `--------------------------------------------.`                
+                `-::::::::::::.     .----------------------------------------------.`               
+                 `--::::::::--`    .------------:oso/--------------/oso:------------.`              
+                   ``.---..``     .-------------shhhh:-------------yhhhs-------------.              
+                      .--.       `--------------:oss+--------------/sss:--------------`             
+                      `---`      .----------------------------------------------------.             
+                       `.--.`    .-----------------------------------------------------`            
+                        `.---.```------------------------------------------------------`            
+                          `..----------------------------------------------------------`            
+                             ``..------------------------------------------------------`            
+                                 .-----------------------------------------------------`            
+                                 `--------------:/--------------------//--------------.             
+                                  .------------+hhs/----------------/shho-------------`             
+                                  `.------------+yhhs+:----------:/shhy+-------------`              
+                                   `.------------:+yhhhyso++++oosyhhyo:-------------.               
+                                    `.--------------:+syyyhhhhyyys+/---------------`                
+                                     `.------------------:://:::-----------------.`                 
+                                       `.--------------------------------------.`                   
+                                         `.----------------------------------.`                     
+                                           `..----------------------------..`                       
+                                              ``..--------------------...`                          
+                                                  ```.....----......``                              
+                                                                                                    
+                                                                                                   
+
+    DOC
+    
     puts "Copyright © 2016 Zach Newburgh"
   end
 end
